d476dabd2646284d39a394d2ed3cba70f49d1aec
Arturas <a.molcanovas@gmail.com>
2019-01-15 16:46:14 +0000

    Anycast @ dropInstance

--- src/dropInstance.ts
+++ src/dropInstance.ts
@@ -2,7 +2,7 @@ import {dropInstanceCommon, executeCallback} from 'localforage-driver-commons';
 import {Store} from './Store';
 
 export function dropInstance(this: any, _options: any, _cb?: any) {
-  const {promise, callback} = dropInstanceCommon.apply(this, arguments);
+  const {promise, callback} = dropInstanceCommon.apply(this, <any>arguments);
 
   const outPromise = (<Promise<string>>promise).then<void>(keyPrefix => {
     Store.resolve(keyPrefix).drop();
