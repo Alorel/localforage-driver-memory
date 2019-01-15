7b25c09a9570ce4b2e6bf972024a68052bc5a495
Arturas <a.molcanovas@gmail.com>
2019-01-15 16:41:23 +0000

    fix: getItem should now return null instead of undefined on a nonexistent value

--- src/getItem.ts
+++ src/getItem.ts
@@ -5,17 +5,10 @@ export function getItem(this: any, key$: string, callback?: any) {
   key$ = normaliseKey(key$);
 
   const promise = this.ready().then(() => {
-    let result = (<Store>this._dbInfo.mStore).get(key$);
+    const result = (<Store>this._dbInfo.mStore).get(key$);
 
-    // If a result was found, parse it from the serialized
-    // string into a JS object. If result isn't truthy, the key
-    // is likely undefined and we'll pass it straight to the
-    // callback.
-    if (result) {
-      result = this._dbInfo.serializer.deserialize(result);
-    }
-
-    return result;
+    // Deserialise if the result is not null or undefined
+    return result == null ? null : this._dbInfo.serializer.deserialize(result); //tslint:disable-line:triple-equals
   });
 
   executeCallback(promise, callback);
